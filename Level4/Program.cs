ae77bfae9e107c2a163f110d720a1ebcbd037c85
marcuszxc <115086076+marcuszxc@users.noreply.github.com>
2022-10-10 15:50:18 +0200

    Add files via upload
    solutions for Level4/Chapter4 in C# Player's Guide.

---
+++ Level4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Querys the Player a qustion aboute what objekt it whas. 
+Console.WriteLine("What kind of thing are we talking about?");
+// Take the answer of the player is given to veribol a. For later use.
+string a = Console.ReadLine();
+// Querys the player aboute atrebuts that describe the objekt.
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+/* 
+ * Takes the answer from the privuse qustion and givs it to veribol b. For later use.
+ * Stores the string "of Doom" in veribol c for later use.
+ * Stores the string "3000" in veribol d for later use.
+ */
+string b = Console.ReadLine();
+string c = "of Doom";
+string d = "3000";
+// Uses all previusly mentiond verbols to construct a sentens.
+Console.WriteLine("The " + b + " " + a + " " + c + " " + d + "!");
